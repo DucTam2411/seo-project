7574147dc80ca947e2fe37a50562e5a0e4dd72f9
Melios025 <84947414+Melios025@users.noreply.github.com>
2022-04-25 02:43:17 +0700

    update tin tuc

--- tintuc/js/main.js
+++ tintuc/js/main.js
@@ -1,3 +1,8 @@
+// load header when have id header
+$("#header").load("../assets/share/headernav.html");
+// load header when have id footer
+$("#footer").load("../assets/share/footer.html");
+
 ;(function () {
 
     'use strict';
