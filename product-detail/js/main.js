82dad590a77f31004dab43880998b424a7ebb36e
sunlvt <101965025+sunlvt@users.noreply.github.com>
2022-04-25 16:26:07 +0700

    Update main.js

--- product-detail/js/main.js
+++ product-detail/js/main.js
@@ -1,4 +1,4 @@
 // load header when have id header
 $("#header").load("../assets/headernav.html");
 // load header when have id footer
-$("#footer").load("./assets/footer.html");
+$("#footer").load("../assets/footer.html");
